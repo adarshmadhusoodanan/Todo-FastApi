57b91a227d03218d89a6194f5b7db522abc5c710
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2025-05-16 21:06:38 +0530

    update logout

--- app/api/endpoints/logout.py
+++ app/api/endpoints/logout.py
@@ -1,4 +1,6 @@
 
+# This endpoint allows users to log out by blacklisting their JWT token.
+# The token is stored in the BlacklistedToken table, and if the user tries to use it again, they will be denied access.
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
@@ -10,6 +12,7 @@ from app.helper.auth import get_current_user , oauth2_scheme
 
 router = APIRouter(tags=["authentication"]) 
 
+# User logout endpoint
 
 @router.post("/logout")
 def logout(current_user: User = Depends(get_current_user),token: str = Depends(oauth2_scheme) ,db: Session = Depends(get_db)):
@@ -34,29 +37,3 @@ def logout(current_user: User = Depends(get_current_user),token: str = Depends(o
     
     return {"detail": f"User {current_user.email} successfully logged out"}
 
-# @router.post("/logout")
-# def logout(
-#     current_user: User = Depends(get_current_user), 
-#     token: str = Depends(oauth2_scheme),
-#     db: Session = Depends(get_db)
-# ):
-#     try:
-#         # Check if token is already blacklisted
-#         statement = select(BlacklistedToken).where(BlacklistedToken.token == token)
-#         existing_token = db.exec(statement).first()
-        
-#         if existing_token:
-#             return {"detail": "Already logged out"}
-        
-#         # Add token to blacklist
-#         blacklisted_token = BlacklistedToken(token=token)
-#         db.add(blacklisted_token)
-#         db.commit()
-        
-#         return {"detail": "Successfully logged out"}
-#     except Exception as e:
-#         db.rollback()  # Rollback the transaction in case of error
-#         raise HTTPException(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             detail=f"Logout failed: {str(e)}"
-#         )
