e53a6f15da47fec82bf30d553e2837361e9631f3
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2025-05-16 20:14:51 +0530

    update routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 
-from app.api.endpoints import create_todo, login, logout, signup
+from app.api.endpoints import completed_todos, create_todo, list_todos, login, logout, mark_todo, signup
 
 
 router = APIRouter()
@@ -12,3 +12,6 @@ router.include_router(login.router)
 router.include_router(logout.router)
 
 router.include_router(create_todo.router)
+router.include_router(list_todos.router)
+router.include_router(completed_todos.router)
+router.include_router(mark_todo.router)
